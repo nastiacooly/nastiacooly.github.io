1a7bf51a265de11925134e2d3117c86f5b7b5cc5
Anastasia <nastia_cool55@gmail.com>
2020-09-16 18:40:20 +0300

    Developed script for sidepanel animation
    Sidepanel now changes color when scrolling the page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const MENU = document.querySelector('.sidemenu'),
     hamburger = document.querySelector('.hamburger'),
-    closeElem = document.querySelector('.sidemenu__menu__close');
+    closeElem = document.querySelector('.sidemenu__menu__close'),
+    sidePanel = document.querySelector('.sidepanel'),
+    sidePanelDivider = document.querySelector('.sidepanel__divider'),
+    sidePanelTitle = document.getElementById('sidepanelTitle');
+
+const svgElements = document.querySelectorAll('a.sidepanel__link > svg > path');
+
+//sidemenu animation
 
 hamburger.addEventListener('click', () => {
     MENU.classList.add('active');
@@ -8,4 +15,23 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     MENU.classList.remove('active');
+});
+
+//sidepanel animation (color change)
+
+document.addEventListener('scroll', () => {
+    if(pageYOffset > 400) {
+        sidePanelTitle.classList.remove('title_whc');
+        sidePanelDivider.classList.add('sidepanel__divider_black');
+        for (let elem of svgElements) {
+            elem.style.fill = "black";
+        }
+    } else {
+        sidePanelTitle.classList.add('title_whc');
+        sidePanelDivider.classList.remove('sidepanel__divider_black');
+        for (let elem of svgElements) {
+            elem.style.fill = "white";
+        }
+    }
+    
 });
