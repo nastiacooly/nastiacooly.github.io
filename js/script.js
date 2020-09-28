8b9123ab403c6f3e42b51ed698d98e2ec23fd115
Anastasia <nastia_cool55@gmail.com>
2020-09-28 17:20:26 +0300

    Progress Bars
    Developed progress bars script which changes inline styles according to the values put in inner HTML

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ const MENU = document.querySelector('.sidemenu'),
     sidePanelTitle = document.getElementById('sidepanelTitle');
 
 const svgElements = document.querySelectorAll('a.sidepanel__link > svg > path'),
-    sideMenuLinks = document.querySelectorAll('ul.sidemenu__menu__list > li > a');
+    sideMenuLinks = document.querySelectorAll('ul.sidemenu__menu__list > li > a'),
+    progressCounters = document.querySelectorAll('.skills__progress__item__head__percentage'),
+    progressScales = document.querySelectorAll('.skills__progress__item__scale > span');
 
 //sidemenu animation
 
@@ -18,11 +20,11 @@ closeElem.addEventListener('click', () => {
     MENU.classList.remove('active');
 });
 
-for (link of sideMenuLinks) {
+sideMenuLinks.forEach(link => {
     link.addEventListener('click', () => {
         MENU.classList.remove('active');
     });
-}
+});
 
 //sidepanel animation (color change)
 
@@ -30,9 +32,9 @@ document.addEventListener('scroll', () => {
     if(pageYOffset > 400) {
         sidePanelTitle.classList.remove('title_whc');
         sidePanelDivider.classList.add('sidepanel__divider_black');
-        for (let elem of svgElements) {
+        svgElements.forEach(elem => {
             elem.style.fill = "black";
-        }
+        });
     } else {
         sidePanelTitle.classList.add('title_whc');
         sidePanelDivider.classList.remove('sidepanel__divider_black');
@@ -42,3 +44,11 @@ document.addEventListener('scroll', () => {
     }
     
 });
+
+//skills progress bars percentage setting
+
+progressCounters.forEach( (counter, i) => {
+    progressScales[i].style.width = counter.innerHTML;
+});
+
+
